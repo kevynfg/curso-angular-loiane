7f77698144965583fc9b028445dedd1c64d33dee
kevyn <kevynredlotus@gmail.com>
2021-02-11 18:36:15 -0300

    - first commit => pipes

--- services/src/app/cursos/cursos.service.ts
+++ services/src/app/cursos/cursos.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Output, EventEmitter } from "@angular/core"
-import {BehaviorSubject} from 'rxjs'
+
+import {LogService} from '../shared/log.service'
 @Injectable()
 export class CursosService {
 
@@ -9,14 +10,16 @@ export class CursosService {
   currentMessage = this.emitirCursoCriado.asObservable()
   private _cursos: string[] = ['Angular 2', 'JavaScript', '.NET', 'Java']
   
-  constructor() {
+  constructor(private logService: LogService) {
     console.log('CursosService instanciada')
   }
   getCursos() {
+    this.logService.consoleLog('Obtendo lista de cursos!')
     return this._cursos
   }
   addCursos(curso: string) {
     if(curso){
+      this.logService.consoleLog(`Criando um novo curso: ${curso}`)
       this._cursos.push(curso)
       this.emitirCursoCriado.emit(curso)
       CursosService.criouNovoCurso.emit(curso)
