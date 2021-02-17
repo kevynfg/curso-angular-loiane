00631c23b9d5572bdb04f8e01d18f97582b40fcf
kevyn <kevynredlotus@gmail.com>
2021-02-17 18:12:41 -0300

    template-driven form, ngModel, FormGroup, FormBuilder

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,20 +8,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
-  user:any = {
-    name: null,
+  user: any = {
+    nome: null,
     email: null
+  };
+
+  onSubmit(formulario: { value: any; form: { reset: () => void; }; }) {
+    console.log(formulario);
+
+    // form.value
+    // console.log(this.usuario);
+
+    this.http.post('https://httpbin.org/post', JSON.stringify(formulario.value))
+      .subscribe(dados => {
+        console.log(dados);
+        formulario.form.reset();
+      });
   }
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
-  onSubmit(form:any) {
-    console.log(form.value)
+  verifyValidTouched(campo:any) {
+    return !campo.valid && campo.touched;
+  }
 
-    console.log(this.user)
+  applyCssError(campo: any) {
+    return {
+      'has-error': this.verifyValidTouched(campo),
+      'has-feedback': this.verifyValidTouched(campo)
+    };
+  }
+
+  findCEP(cep:any, form:any) {
+    // Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != null && cep !== '') {
+      cep.consultaCEP(cep)
+      .subscribe((dados: any) => this.fillDataFields(dados, form));
+    }
+  }
+
+  fillDataFields(dados:any, formulario:any) {
+    /*formulario.setValue({
+      nome: formulario.value.nome,
+      email: formulario.value.email,
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        numero: '',
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });*/
+
+    formulario.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        // cep: dados.cep,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+
+    // console.log(form);
+  }
+
+  resetaDadosForm(formulario:any) {
+    formulario.form.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
 }
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
 
 import { TemplateFormComponent } from './template-form.component';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { CampoControlErrorComponent } from '../campo-control-error/campo-control-error.component';
 
 
 
 @NgModule({
   declarations: [
-    TemplateFormComponent
+    TemplateFormComponent,
+    FormDebugComponent,
+    CampoControlErrorComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class TemplateFormModule { }
